459905982c07a663f475bbe7c4b89db50d36b2c4
Kristian Epps <kristian.epps@bbc.co.uk>
2016-03-09 14:35:10 +0000

    done it

--- index.js
+++ index.js
@@ -2,20 +2,19 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 var Client = require('node-rest-client').Client;
+var amqp = require('amqplib/callback_api');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.post('/json', function(req, res) {
-
-    var client = new Client();
-    var args = {
-        data: req.body,
-        headers: { "Content-Type": "application/json" }
-    };
-
-    client.post("http://localhost:3001/json", args, function (data, response) {
-        res.status(200).json(data);
+    amqp.connect('amqp://localhost', function(err, conn) {
+        conn.createChannel(function(err, ch) {
+            var q = 'queue';
+            ch.assertQueue(q, {durable: false});
+            ch.sendToQueue(q, new Buffer(JSON.stringify(req.body)));
+            console.log(" [x] Sent Data");
+        });
     });
 });
 
@@ -25,6 +24,7 @@ app.get('/tennis-events', function(req, res) {
         res.status(200).json(data);
     });
 });
+
 
 
 module.exports = app;
